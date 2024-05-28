a82ad24697d0fe0379978647b2a0fb9125631ca7
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-05-28 08:11:59 -0400

    Post not found

---
+++ app/(pages)/updates/not-found.tsx
@@ -0,0 +1,18 @@
+import { faArrowRight } from '@awesome.me/kit-d7ccc5bb1a/icons/classic/solid'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Link from 'next/link'
+
+export default function NotFound() {
+	return (
+		<div className="flex flex-col items-center justify-center w-full gap-4 mt-2">
+			<div className="flex flex-col w-full max-w-lg gap-2 p-4 px-8 mb-8 text-left border rounded-lg border-neutral-900 bg-neutral-950/75">
+				<h1 className="text-6xl font-bold text-brand2">Uh oh...</h1>
+				<h2 className="text-xl text-white/80">These aren&apos;t the droids you&apos;re looking for...</h2>
+				<Link href="/updates" className="flex flex-row items-center justify-center gap-2 text-2xl font-bold text-brand1 w-fit">
+					<span className="link">Go back...</span>
+					<FontAwesomeIcon icon={faArrowRight} />
+				</Link>
+			</div>
+		</div>
+	)
+}
--- app/(pages)/updates/page.tsx
+++ app/(pages)/updates/page.tsx
@@ -8,6 +8,7 @@ export default async function UpdatesPage() {
 	return (
 		<div className="w-full max-w-3xl mb-8 border rounded-lg border-neutral-900 bg-neutral-950/75">
 			<div className="flex flex-col items-center justify-center w-full p-2 divide-y divide-brand2">
+				{posts.length === 0 && <div className="text-center">No posts found</div>}
 				{posts.map(post => {
 					return <PostRow key={post._id} post={post as NonNullable<POST_QUERYResult>} />
 				})}
