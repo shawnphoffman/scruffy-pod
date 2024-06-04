8a28b9a411f687f81c2e21b2378ca4fe236cfa63
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-06-04 19:19:06 -0400

    title size

--- components/updates/PostTitle.tsx
+++ components/updates/PostTitle.tsx
@@ -1,6 +1,6 @@
 export default function PostTitle({ children }) {
 	return (
-		<h1 className="mb-2 text-6xl font-bold leading-tight tracking-tighter text-center text-brand2 md:text-7xl md:leading-none lg:text-8xl text-balance">
+		<h1 className="w-full mb-2 text-4xl font-bold leading-tight tracking-tighter text-center sm:text-5xl md:text-6xl lg:text-8xl text-brand2 md:leading-none text-balance">
 			{children}
 		</h1>
 	)
