849df11440f3d166e9591b16f6858fcaf9a4c34f
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-06-11 17:15:09 -0400

    Embed comp

--- components/updates/portableText/PostImage.tsx
+++ components/updates/portableText/PostImage.tsx
@@ -30,7 +30,7 @@ const PostImage = (props: PostImageProps) => {
 				//
 				sizes="(max-width: 800px) 100vw, 800px"
 				className={classnames(
-					'mw-full h-auto max-h-96 aspect-auto w-auto mx-auto hover:cursor-pointer hover:outline outline-brand-blue outline-offset-2',
+					'mw-full h-auto max-h-96 aspect-auto w-auto mx-auto hover:cursor-pointer hover:outline rounded outline-brand-blue outline-offset-2',
 					props.className
 				)}
 				onClick={() => setOpen(true)}
