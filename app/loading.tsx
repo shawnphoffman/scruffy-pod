a78647853c052624837902d7268a870712b1399b
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-06-24 10:37:31 -0700

    Update loading icon to be dice

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,12 @@
-import { faSpaceStationMoonConstruction } from '@awesome.me/kit-d7ccc5bb1a/icons/classic/solid'
+// import { faFalcon, faScruffyDice } from '@awesome.me/kit-d7ccc5bb1a/icons/kit/custom'
+import { faScruffyDice } from '@awesome.me/kit-d7ccc5bb1a/icons/kit/custom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Loading() {
 	return (
-		<div className="text-4xl text-white">
-			<FontAwesomeIcon icon={faSpaceStationMoonConstruction} beatFade />
+		<div className="text-4xl text-brand-yellow">
+			<FontAwesomeIcon icon={faScruffyDice} beatFade size="2x" />
+			{/* <FontAwesomeIcon icon={faFalcon} beatFade /> */}
 		</div>
 	)
 }
