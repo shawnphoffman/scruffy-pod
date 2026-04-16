e54f63a3094344e48f0481dfcb9997f4adb8bf5a
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2026-04-16 13:36:14 -0700

    Add SiteBanner component and implement banner query
    - Introduced the SiteBanner component in layout.tsx for improved site presentation.
    - Added BANNER_QUERY to fetch site banner data from Sanity.
    - Updated sanity.requests.ts to include a function for retrieving the banner data.

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import Image from 'next/image'
 import { siteDescription as description, siteTitle, siteUrl as url } from '@/app/meta'
 import headerImage from '@/app/title.png'
 import ActiveLink from '@/components/core/ActiveLink'
+import SiteBanner from '@/components/core/SiteBanner'
 import StarBackground from '@/components/core/StarBackground'
 
 export const metadata = {
@@ -36,9 +37,10 @@ export default function RootLayout({ children }) {
 			<head>
 				<meta name="apple-itunes-app" content="app-id=1272718319" />
 			</head>
-			<body className="px-2 py-0 mx-auto my-0 text-white min-h-dvh w-dvw">
+			<body className="p-0 mx-auto my-0 text-white min-h-dvh w-dvw">
 				<StarBackground />
-				<div className="flex flex-col items-center w-full max-w-screen-xl mx-auto">
+				<SiteBanner />
+				<div className="flex flex-col items-center w-full max-w-screen-xl px-2 mx-auto">
 					<div className="flex flex-col w-full max-w-4xl min-h-dvh">
 						<div className="flex flex-col items-center m-4 mb-2 text-center">
 							<h1 className="sr-only">{siteTitle}</h1>
