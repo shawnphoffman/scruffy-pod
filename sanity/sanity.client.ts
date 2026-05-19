9d085ed8d6f970f3fa216a2a03a48f85f674bc07
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2026-05-19 08:12:54 -0700

    feat: route sanity client/image/queries/types through pod-sites-shared

--- sanity/sanity.client.ts
+++ sanity/sanity.client.ts
@@ -1,11 +1,10 @@
-import { createClient } from 'next-sanity'
+import { createSanityClient } from '@shawnphoffman/pod-sites-shared/sanity'
 
 import { apiVersion, dataset, projectId, useCdn } from './sanity.env'
 
-export default createClient({
+export default createSanityClient({
 	apiVersion,
 	dataset,
 	projectId,
 	useCdn,
-	perspective: 'published',
 })
